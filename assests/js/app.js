ee7b6dab65ab65b3dfa2b1643d914292c096299e
abdulwahabahmedkhanyusufzai <absurw@gmail.com>
2024-12-17 18:44:32 +0500

    Comitted

--- assests/js/app.js
+++ assests/js/app.js
@@ -13,3 +13,19 @@ if(navClose){
         navMenu.classList.remove('show-menu')
     })
 }
+navLink.forEach(n => n.addEventListener('click',linkAction))
+
+const shadowHeader = () =>{
+    const header=document.getElementById('header')
+    
+    this.scrollY >=50 ? header.classList.add('shadow-header')
+                      :header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll' , shadowHeader)
+
+const swiperPopular= new Swiper('.popular__swiper',{
+    loop:true,
+    grabCursor:true,
+    slidesPerView:'auto',
+    centeredSlides:'auto',
+})
